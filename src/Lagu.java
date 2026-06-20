ada0ab126f47c6a9338c9e683f35ecc02fb20f7a
Shandy.Saputra <shandy.saputra@binus.ac.id>
2026-06-20 12:14:39 +0700

    Pembuatan Class Lagu

---
+++ src/Lagu.java
@@ -0,0 +1,48 @@
+public class Lagu {
+    private String judul;
+    private String artis;
+    private double durasi;
+
+    // Constructor
+    public Lagu(String judul, String artis, double durasi){
+        this.judul = judul;
+        this.artis = artis;
+        this.durasi = durasi;
+    }
+
+    public Lagu(){
+
+    }
+
+    // Getter and Setter
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getArtis() {
+        return artis;
+    }
+
+    public void setArtis(String artis) {
+        this.artis = artis;
+    }
+
+    public double getDurasi() {
+        return durasi;
+    }
+
+    public void setDurasi(double durasi) {
+        this.durasi = durasi;
+    }
+
+    // Method untuk show Lagu
+    public String tampilkanInfo(){
+        return "Judul\t: " + getJudul() +
+                "\nArtis\t: " + getArtis() +
+                "\nDurasi\t: " +getDurasi();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main{
+    public static void main(String[] args){
+
+        // Test untuk class lagu
+        // Contoh implementasi enkapsulasi
+        Lagu[] playlist = new Lagu[5];
+
+        playlist[0] = new Lagu();
+        playlist[0].setJudul("Ini Judul 1");
+        playlist[0].setArtis("Ini Artis 1");
+        playlist[0].setDurasi(3);
+
+        playlist[1] = new Lagu();
+        playlist[1].setJudul("Ini Judul 2");
+        playlist[1].setArtis("Ini Artis 2");
+        playlist[1].setDurasi(4);
+
+        playlist[2] = new Lagu();
+        playlist[2].setJudul("Ini Judul 3");
+        playlist[2].setArtis("Ini Artis 3");
+        playlist[2].setDurasi(3);
+
+        playlist[3] = new Lagu();
+        playlist[3].setJudul("Ini Judul 4");
+        playlist[3].setArtis("Ini Artis 4");
+        playlist[3].setDurasi(4);
+
+        playlist[4] = new Lagu();
+        playlist[4].setJudul("Ini Judul 5");
+        playlist[4].setArtis("Ini Artis 5");
+        playlist[4].setDurasi(3);
+
+        // Show playlist
+        System.out.println("=== Playlist ===");
+        for (int i = 0; i < playlist.length; i++){
+            System.out.println("Lagu ke-" + (i+1));
+            System.out.println(playlist[i].tampilkanInfo());
+        }
+    }
+}
